16f805091ba989dd4bdcbdca3dab523b6c63f165
Stephanie McCumsey <smccumsey@gmail.com>
2016-11-30 17:19:34 +0000

    checkout admin.py

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,73 @@ from django.contrib import admin
 from .models import Course, Module, Assessment, Content, Related, Video, AssessmentSubmission
 from .models import Student, CourseProgress, ModuleProgress, AssessmentProgress, VideoProgress, ContentProgress
 
+class AssessmentFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = ('module name')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'name'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('zero', ('in Module 0')),
+            ('one', ('in Module 1')),
+            ('two', ('in Module 2')),
+            ('three', ('in Module 3')),
+            ('four', ('in Module 4')),
+            ('five', ('in Module 5')),
+            ('six', ('in Module 6')),
+            ('seven', ('in Module 7')),
+            ('eight', ('in Module 8')),
+            ('nine', ('in Module 9')),
+            ('ten', ('in Module 10')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'zero':
+            return queryset.filter(assessment__module__title__contains = "0. ")
+        if self.value() == 'one':
+            return queryset.filter(assessment__module__title__contains = "1. ")
+        if self.value() == 'two':
+            return queryset.filter(assessment__module__title__contains = "2. ")
+        if self.value() == 'three':
+            return queryset.filter(assessment__module__title__contains = "3. ")
+        if self.value() == 'four':
+            return queryset.filter(assessment__module__title__contains = "4. ")
+        if self.value() == 'five':
+            return queryset.filter(assessment__module__title__contains = "5. ")
+        if self.value() == 'six':
+            return queryset.filter(assessment__module__title__contains = "6. ")
+        if self.value() == 'seven':
+            return queryset.filter(assessment__module__title__contains = "7. ")
+        if self.value() == 'eight':
+            return queryset.filter(assessment__module__title__contains = "8. ")
+        if self.value() == 'nine':
+            return queryset.filter(assessment__module__title__contains = "9. ")
+        if self.value() == 'ten':
+            return queryset.filter(assessment__module__title__contains = "10. ")
+
+class AssessmentProgressAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'solved')
+    list_filter = (AssessmentFilter,)
+    model = AssessmentProgress
+    ordering = ['assessment']
+
 admin.site.register(Course)
 admin.site.register(Module)
 admin.site.register(Assessment)
@@ -12,6 +79,7 @@ admin.site.register(Student)
 admin.site.register(CourseProgress)
 admin.site.register(ModuleProgress)
 admin.site.register(ContentProgress)
-admin.site.register(AssessmentProgress)
+admin.site.register(AssessmentProgress,AssessmentProgressAdmin)
 admin.site.register(VideoProgress)
 admin.site.register(AssessmentSubmission)
+
